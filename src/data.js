d0845d6d5b53f53a0b3b7b7e92a52db931fb2eee
Oscar Dario <56013454+elpuchito@users.noreply.github.com>
2024-03-23 21:19:18 -0500

    uploaded songs 1

--- src/data.js
+++ src/data.js
@@ -11,23 +11,107 @@ function data() {
       id: uuidv4(),
       active: true
     },
+    
     {
-      name: 'Bachata En Fukuoka 2',
+      name: 'Apaga y Vamonos',
       cover: 'https://ia804605.us.archive.org/0/items/09-frio-frio/cover.jpg?cnt=0',
       artist: 'Juan Luis Guerra',
-      audio: '11_-_Bachata_En_Fukuoka.mp3',
-      color: [ '#CD607D', '#c94043' ],
-      id: uuidv4(),
-      active: true
-    },
-    {
-      name: 'Bachata En Fukuoka 2',
-      cover: 'https://ia804605.us.archive.org/0/items/09-frio-frio/cover.jpg?cnt=0',
-      artist: 'Juan Luis Guerra link',
       audio: 'https://ia804605.us.archive.org/0/items/09-frio-frio/01%20-%20Apaga%20Y%20V%C3%A1monos%20%28En%20Vivo%20Estadio%20Ol%C3%ADmpico%20De%20Rep%C3%BAblica%20Dominicana_2012%29.mp3',
       color: [ '#CD607D', '#c94043' ],
       id: uuidv4(),
-      active: true
+      active: false
+    },
+    {
+      name: 'La Llave De Mi Corazón',
+      cover: 'https://ia804605.us.archive.org/0/items/09-frio-frio/cover.jpg?cnt=0',
+      artist: 'Juan Luis Guerra',
+      audio: 'audio/juan-luis/04_-_La_Llave_De_Mi_Corazon.mp3',
+      color: [ '#CD607D', '#c94043' ],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: 'Ojalá Que Llueva Café ',
+      cover: 'https://ia804605.us.archive.org/0/items/09-frio-frio/cover.jpg?cnt=0',
+      artist: 'Juan Luis Guerra',
+      audio: 'audio/juan-luis/14_-_Ojala_Que_Llueva_Cafe.mp3',
+      color: [ '#CD607D', '#c94043' ],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: 'Mi Bendición',
+      cover: 'https://ia804605.us.archive.org/0/items/09-frio-frio/cover.jpg?cnt=0',
+      artist: 'Juan Luis Guerra',
+      audio: 'audio/juan-luis/05_-_Mi_Bendicion.mp3',
+      color: [ '#CD607D', '#c94043' ],
+      id: uuidv4(),
+      active: false
+    },
+    
+    {
+      name: 'El Niágra En Bicicleta',
+      cover: 'https://ia804605.us.archive.org/0/items/09-frio-frio/cover.jpg?cnt=0',
+      artist: 'Juan Luis Guerra',
+      audio: 'audio/juan-luis/07_-_El_Niagra_En_Bicicleta.mp3',
+      color: [ '#CD607D', '#c94043' ],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: 'A Pedir Su Mano',
+      cover: 'https://ia804605.us.archive.org/0/items/09-frio-frio/cover.jpg?cnt=0',
+      artist: 'Juan Luis Guerra',
+      audio: 'audio/juan-luis/A_Pedir_Su_Mano__Juan_Luis_Guerra.mp3',
+      color: [ '#CD607D', '#c94043' ],
+      id: uuidv4(),
+      active: false
+    },
+    
+    {
+      name: 'Cuando Nos Volvamos a Encontrar',
+      cover: 'https://upload.wikimedia.org/wikipedia/en/a/ae/Corazon_Profundo_cover.jpg',
+      artist: 'Carlos Vives',
+      audio: 'audio/carlosvives/02 Cuando Nos Volvamos A Encontrar.mp3',
+      color: [ '#CD607D', '#c94043' ],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: 'Ella Es Mi Fiesta',
+      cover: 'https://upload.wikimedia.org/wikipedia/en/a/ae/Corazon_Profundo_cover.jpg',
+      artist: 'Carlos Vives',
+      audio: 'audio/carlosvives/04 Ella Es Mi Fiesta.mp3',
+      color: [ '#CD607D', '#c94043' ],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: 'Somos Dos',
+      cover: 'https://geo-media.beatsource.com/image_size/500x500/c/f/2/cf2e6439-41b9-45f2-8153-83dcea955733.jpg',
+      artist: 'Bomba Estereo',
+      audio: 'audio/carlosvives/Bomba Estereo Somos Dos.mp3',
+      color: [ '#CD607D', '#c94043' ],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: 'Tu Si',
+      cover: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqP-MklHuki2eHy1LsK4aLi2eAEBSlRmy4PP4JeYEwJDxHKJXL9B4tRacIzu95LZbdo8Q&usqp=CAU',
+      artist: 'legend Effect',
+      audio: 'audio/carlosvives/Legend Effect Tu Si El Gerard Gaby.mp3',
+      color: [ '#CD607D', '#c94043' ],
+      id: uuidv4(),
+      active: false
+    },
+    {
+      name: 'ADMV',
+      cover: 'https://i1.sndcdn.com/artworks-2o7arVUF72Up-0-t500x500.jpg',
+      artist: 'Maluma',
+      audio: 'audio/carlosvives/Maluma - ADMV.mp3',
+      color: [ '#CD607D', '#c94043' ],
+      id: uuidv4(),
+      active: false
     },
   ];
 }
