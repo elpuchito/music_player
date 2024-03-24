9b2f62a8c125cef7434c022f9d7bb1b929c84311
Oscar Dario <56013454+elpuchito@users.noreply.github.com>
2024-03-23 20:58:56 -0500

    test link

--- src/data.js
+++ src/data.js
@@ -2,17 +2,33 @@ import { v4 as uuidv4 } from "uuid";
 function data() {
   return [
     //TOP 10
-
     {
       name: 'Bachata En Fukuoka',
       cover: 'https://ia804605.us.archive.org/0/items/09-frio-frio/cover.jpg?cnt=0',
       artist: 'Juan Luis Guerra',
-      audio: '../public/audio/juan-luis/11_-_Bachata_En_Fukuoka.mp3',
+      audio: 'audio/juan-luis/11_-_Bachata_En_Fukuoka.mp3',
       color: [ '#CD607D', '#c94043' ],
       id: uuidv4(),
       active: true
     },
-    
+    {
+      name: 'Bachata En Fukuoka 2',
+      cover: 'https://ia804605.us.archive.org/0/items/09-frio-frio/cover.jpg?cnt=0',
+      artist: 'Juan Luis Guerra',
+      audio: '11_-_Bachata_En_Fukuoka.mp3',
+      color: [ '#CD607D', '#c94043' ],
+      id: uuidv4(),
+      active: true
+    },
+    {
+      name: 'Bachata En Fukuoka 2',
+      cover: 'https://ia804605.us.archive.org/0/items/09-frio-frio/cover.jpg?cnt=0',
+      artist: 'Juan Luis Guerra link',
+      audio: 'https://ia804605.us.archive.org/0/items/09-frio-frio/01%20-%20Apaga%20Y%20V%C3%A1monos%20%28En%20Vivo%20Estadio%20Ol%C3%ADmpico%20De%20Rep%C3%BAblica%20Dominicana_2012%29.mp3',
+      color: [ '#CD607D', '#c94043' ],
+      id: uuidv4(),
+      active: true
+    },
   ];
 }
 
