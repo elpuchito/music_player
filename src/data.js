4c23fb909ff9fa55f3fe3902a885ab7337b36daa
Oscar Dario <56013454+elpuchito@users.noreply.github.com>
2024-03-23 20:33:00 -0500

    fixed path 1

--- src/data.js
+++ src/data.js
@@ -7,10 +7,10 @@ function data() {
       name: 'Bachata En Fukuoka',
       cover: 'https://ia804605.us.archive.org/0/items/09-frio-frio/cover.jpg?cnt=0',
       artist: 'Juan Luis Guerra',
-      audio: 'public/audio/juan-luis/11_-_Bachata_En_Fukuoka.mp3',
+      audio: '../public/audio/juan-luis/11_-_Bachata_En_Fukuoka.mp3',
       color: [ '#CD607D', '#c94043' ],
       id: 'uuidv4()',
-      active: false
+      active: true
     },
     
   ];
